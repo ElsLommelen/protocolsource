f9f2276fa4f36ce6273be2cb9f880ded6162f8ac
ElsLommelen <els.lommelen@inbo.be>
2022-04-01 17:53:58 +0200

    operational docker workflow (locally)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,10 @@
 #!/bin/sh -l
 
 echo '\nGetting the code...\n'
-git clone --quiet https://$2@github.com/$1 /render
+git clone --quiet https://$INPUT_TOKEN@github.com/$GITHUB_REPOSITORY_SOURCE /render
 cd /render
-git config advice.detachedHead false
-if [ $3 -eq 0 ] ; then
-  echo 'Checking out '$GITHUB_SHA'...\n'
-  git checkout $GITHUB_SHA
-else
-  echo 'Checking out main...\n'
-  git checkout main
-fi
-cp -r /render/. $GITHUB_WORKSPACE
-cd $GITHUB_WORKSPACE
+git checkout $BRANCH_SOURCE
+
 echo 'Rendering the Rmarkdown files...\n'
 rm .Rprofile
 Rscript "render.R"
@@ -22,23 +14,19 @@ if [ $? -ne 0 ]; then
 fi
 echo '\nAll Rmarkdown files rendered successfully\n'
 
-if [ $3 -eq 0 ]; then
-  echo 'Done.\n'
-  exit 0
-fi
-
 echo 'Publishing the rendered files...\n'
-git clone --branch=publish https://$2@github.com/$1 /publish
+git clone --quiet --depth=1 --single-branch --branch=main https://$INPUT_TOKEN@github.com/$GITHUB_REPOSITORY_DEST /destiny
 git config --global user.email "info@inbo.be"
 git config --global user.name "INBO"
-cd /publish
-git rm -r .
-cp -R $GITHUB_WORKSPACE/publish/. .
-if ! git diff-index --quiet HEAD --; then
-    git add --all
-    git commit --amend --message="Te publiceren versie van de NARA-2020 achtergronddocumenten"
-    git push -f
-    echo '\nNew version published...'
-else
-  echo '\nNothing to update...'
-fi
+
+cp -R /render/publish/. /destiny/.
+cd /destiny
+ls -a
+
+git config user.name
+git config user.email
+git add --all
+git commit --message="Nieuwe protocollen toegevoegd"
+git push -f https://$INPUT_TOKEN@github.com/$GITHUB_REPOSITORY_DEST
+
+echo '\nNew version published...'
