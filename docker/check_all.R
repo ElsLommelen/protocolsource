e4cf9f6ef7b9d90c91bde00e7f499ebae676c41f
ElsLommelen <els.lommelen@inbo.be>
2022-07-18 17:37:27 +0200

    test checklist::clean_git()

--- docker/check_all.R
+++ docker/check_all.R
@@ -1,7 +1,8 @@
 # bundled checks for protocols
 library(protocolhelper)
 check_all <- function(protocol_code) {
-  sapply(protocol_code, protocolhelper::check_frontmatter)
+  checklist::clean_git()
+  #sapply(protocol_code, protocolhelper::check_frontmatter)
   sapply(protocol_code, protocolhelper::check_structure)
   #return(invisible(protocol_code))
 }
