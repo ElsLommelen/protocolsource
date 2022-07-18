cb63ddbcae4cccef3a70bedc23e4a30b96fbfc89
ElsLommelen <els.lommelen@inbo.be>
2022-07-18 15:04:04 +0200

    new test check_structure()

--- docker/check_all.R
+++ docker/check_all.R
@@ -1,9 +1,8 @@
 # bundled checks for protocols
 library(protocolhelper)
 check_all <- function(protocol_code) {
-  stop("Test-error")
   #sapply(protocol_code, protocolhelper::check_frontmatter)
-  #sapply(protocol_code, protocolhelper::check_structure)
+  sapply(protocol_code, protocolhelper::check_structure)
   #return(invisible(protocol_code))
 }
 check_all(Sys.getenv("PROTOCOL_CODE"))
