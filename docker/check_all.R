97fe1aa6874295dd49e20e3386b83e75a8ef0505
ElsLommelen <els.lommelen@inbo.be>
2022-07-18 17:05:19 +0200

    check_frontmatter terug toevoegen

--- docker/check_all.R
+++ docker/check_all.R
@@ -1,7 +1,7 @@
 # bundled checks for protocols
 library(protocolhelper)
 check_all <- function(protocol_code) {
-  #sapply(protocol_code, protocolhelper::check_frontmatter)
+  sapply(protocol_code, protocolhelper::check_frontmatter)
   sapply(protocol_code, protocolhelper::check_structure)
   #return(invisible(protocol_code))
 }
