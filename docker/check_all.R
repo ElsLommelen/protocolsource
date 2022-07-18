9e8ad681a307691d983e8f6b4e01f96de510a198
ElsLommelen <els.lommelen@inbo.be>
2022-07-18 14:17:40 +0200

    test check_structure() instead of check_frontmatter()

--- docker/check_all.R
+++ docker/check_all.R
@@ -1,8 +1,9 @@
 # bundled checks for protocols
 library(protocolhelper)
 check_all <- function(protocol_code) {
-  sapply(protocol_code, protocolhelper::check_frontmatter)
-  #sapply(protocolcode, protocolhelper::check_structure)
-  return(invisible(protocol_code))
+  stop("Test-error")
+  #sapply(protocol_code, protocolhelper::check_frontmatter)
+  #sapply(protocol_code, protocolhelper::check_structure)
+  #return(invisible(protocol_code))
 }
 check_all(Sys.getenv("PROTOCOL_CODE"))
