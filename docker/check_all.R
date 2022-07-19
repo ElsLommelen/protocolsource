46ddfbfed53fa354155a233a27c6142b94fb93c6
ElsLommelen <els.lommelen@inbo.be>
2022-07-19 10:30:05 +0200

    test of interactive() de zaak oplost

--- docker/check_all.R
+++ docker/check_all.R
@@ -1,7 +1,9 @@
 # bundled checks for protocols
 library(protocolhelper)
 check_all <- function(protocol_code) {
-  checklist::clean_git()
+  if (interactive()) {
+    checklist::clean_git()
+  }
   #sapply(protocol_code, protocolhelper::check_frontmatter)
   sapply(protocol_code, protocolhelper::check_structure)
   #return(invisible(protocol_code))
