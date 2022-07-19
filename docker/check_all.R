92bfb478f25a0d3194b4925303013050ac268470
ElsLommelen <els.lommelen@inbo.be>
2022-07-19 16:09:56 +0200

    rewrite check_all as function

--- docker/check_all.R
+++ docker/check_all.R
@@ -1,16 +1,19 @@
 # bundled checks for protocols
-protocol_code <- Sys.getenv("PROTOCOL_CODE")
-fail <- FALSE
-tryCatch(
-  protocolhelper::check_frontmatter(protocol_code),
-  error = function(e) paste0(e, "\n\n"),
-  fail = TRUE
-)
-tryCatch(
-  protocolhelper::check_structure(protocol_code),
-  error = function(e) e,
-  fail = TRUE
-)
-if (fail) {
-  stop("\nThe source code failed some checks. Please check the error message above.\n")
+check_all <- function(protocol_code) {
+  #protocol_code <- Sys.getenv("PROTOCOL_CODE")
+  fail <- FALSE
+  tryCatch(
+    protocolhelper::check_frontmatter(protocol_code),
+    error = function(e) paste0(e, "\n\n"),
+    fail = TRUE
+  )
+  tryCatch(
+    protocolhelper::check_structure(protocol_code),
+    error = function(e) e,
+    fail = TRUE
+  )
+  if (fail) {
+    stop("\nThe source code failed some checks. Please check the error message above.\n")
+  }
 }
+  
