ca066359bbcc77bcb9b8d6c89002f889ef064c1d
ElsLommelen <els.lommelen@inbo.be>
2022-07-19 15:45:13 +0200

    trycatch in check_all.R

--- docker/check_all.R
+++ docker/check_all.R
@@ -1,6 +1,17 @@
 # bundled checks for protocols
 library(protocolhelper)
 protocol_code <- Sys.getenv("PROTOCOL_CODE")
-  sapply(protocol_code, protocolhelper::check_frontmatter)
-  sapply(protocol_code, protocolhelper::check_structure)
-  #return(invisible(protocol_code))
+fail <- FALSE
+tryCatch(
+  protocolhelper::check_frontmatter(protocol_code),
+  error = function(e) e,
+  fail = TRUE
+)
+tryCatch(
+  protocolhelper::check_structure(protocol_code),
+  error = function(e) e,
+  fail = TRUE
+)
+if (fail) {
+  stop("\nThe source code failed some checks. Please check the error message above.\n")
+}
