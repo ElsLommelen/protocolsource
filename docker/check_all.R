30052ac61b6cc3f01d48b8f7ad020efdef091867
ElsLommelen <els.lommelen@inbo.be>
2022-07-19 11:27:54 +0200

    check_frontmatter() testen i.p.v. tweemaal check_structure()

--- docker/check_all.R
+++ docker/check_all.R
@@ -1,7 +1,6 @@
 # bundled checks for protocols
 library(protocolhelper)
 protocol_code <- Sys.getenv("PROTOCOL_CODE")
-  #sapply(protocol_code, protocolhelper::check_frontmatter)
-  sapply(protocol_code, protocolhelper::check_structure)
+  sapply(protocol_code, protocolhelper::check_frontmatter)
   sapply(protocol_code, protocolhelper::check_structure)
   #return(invisible(protocol_code))
