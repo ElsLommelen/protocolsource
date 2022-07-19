5406827752f7b4c945267133d3c012f8fcd95caf
ElsLommelen <els.lommelen@inbo.be>
2022-07-19 16:55:41 +0200

    en hu is het hopelijk echt in orde...

--- docker/check_all.R
+++ docker/check_all.R
@@ -3,18 +3,14 @@ check_all <- function(protocol_code) {
   #protocol_code <- Sys.getenv("PROTOCOL_CODE")
   fail <- FALSE
   tryCatch(
-    protocolhelper::check_frontmatter(protocol_code, fail = TRUE),
-    error = function(e) {
-      e
-      fail = TRUE
-    }
+    protocolhelper::check_frontmatter(protocol_code),
+    error = function(e) e,
+    finally = fail <- TRUE
   )
   tryCatch(
-    protocolhelper::check_structure(protocol_code, fail = TRUE),
-    error = function(e) {
-      paste0("\n\n", e)
-      fail = TRUE
-    }
+    protocolhelper::check_structure(protocol_code),
+    error = function(e) paste0("\n\n", e),
+    finally = fail <- TRUE
   )
   if (fail) {
     stop("\nThe source code failed some checks. Please check the error message above.\n")
