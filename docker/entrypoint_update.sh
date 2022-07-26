4285032f07087f38255792019698cee1a3d491ee
ElsLommelen <els.lommelen@inbo.be>
2022-07-26 15:51:26 +0200

    verwijder overbodige (test)code

--- docker/entrypoint_update.sh
+++ docker/entrypoint_update.sh
@@ -1,13 +1,10 @@
 #!/bin/sh -l
 
 echo '\nGetting the code...\n'
-git clone --branch=$BRANCH_SOURCE https://$INPUT_TOKEN_INBO@github.com/$GITHUB_REPOSITORY /update
+git clone --branch=$BRANCH_SOURCE https://$INPUT_TOKEN@github.com/$GITHUB_REPOSITORY /update
 git config --global user.email "info@inbo.be"
 git config --global user.name "INBO"
 cd /update
-#git checkout $BRANCH_SOURCE
-ls -a
-rm .Rprofile
 
 echo '\nUpdating zenodo...\n'
 Rscript --no-save --no-restore -e 'protocolhelper:::update_zenodo()'
@@ -16,19 +13,9 @@ git commit --message="update .zenodo.json"
 
 echo '\nUpdating general NEWS.md...\n'
 Rscript --no-save --no-restore -e 'protocolhelper:::update_news_release("'$PROTOCOL_CODE'")'
-#git config user.name
-#git config user.email
 git add --all
 git commit --message="update general NEWS.md"
 
-echo 'git config --list:'
-git config --list
-
-echo 'git branch'
-git branch
-
-echo 'git remote -v show origin'
-git remote -v show origin
 
 echo 'git push'
 git push -f
