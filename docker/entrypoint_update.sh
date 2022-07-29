e8860b088d9d54b302dd02a2ec34db73b9830365
INBO <info@inbo.be>
2022-07-29 13:12:51 +0200

    undo handleiding door renv -> renv::restore() niet meer nodig voor entrypoint_update()

--- docker/entrypoint_update.sh
+++ docker/entrypoint_update.sh
@@ -6,20 +6,20 @@ git config --global user.email "info@inbo.be"
 git config --global user.name "INBO"
 cd /update
 
+rm .Rprofile
+
 echo '\nSession info\n'
-Rscript -e "renv::restore()"
 Rscript -e 'sessioninfo::session_info()'
 
 echo '\nUpdating zenodo...\n'
 Rscript --no-save --no-restore -e 'protocolhelper:::update_zenodo()'
-git add --all
+git add .zenodo.json
 git commit --message="update .zenodo.json"
 
 echo '\nUpdating general NEWS.md...\n'
 Rscript --no-save --no-restore -e 'protocolhelper:::update_news_release("'$PROTOCOL_CODE'")'
-git add --all
+git add NEWS.md
 git commit --message="update general NEWS.md"
-
 
 echo 'git push'
 git push -f
