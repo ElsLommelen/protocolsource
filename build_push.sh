5b5595af8a0225bad124f5d4cf7c08d9fefcb382
INBO <info@inbo.be>
2022-08-22 13:43:19 +0200

    verduidelijking in build_push.sh

--- build_push.sh
+++ build_push.sh
@@ -1,3 +1,20 @@
+#to be used on protocolsource (change entrypoint below in Dockerfile to run other parts)
+
+docker build --tag=inbo/protocols .
+
+#entrypoint_check
+docker run --rm --env GITHUB_REPOSITORY="ElsLommelen/protocolsource" --env BRANCH_SOURCE="dockertests" --env PROTOCOL_CODE="sfp-401-nl" --env INPUT_TOKEN inbo/protocols
+#INPUT_TOKEN is als omgevingsvariabele toegevoegd op laptop
+
+#entrypoint_update
+docker run --rm --env GITHUB_REPOSITORY="ElsLommelen/protocolsource" --env BRANCH_SOURCE="dockertests" --env PROTOCOL_CODE="sfp-401-nl" --env INPUT_TOKEN inbo/protocols
+
+#entrypoint_website
+docker run --rm --env GITHUB_REPOSITORY_SOURCE="ElsLommelen/protocolsource" --env BRANCH_SOURCE="dockertests" --env GITHUB_REPOSITORY_DEST="ElsLommelen/protocols" --env INPUT_TOKEN --env GITHUB_ACTIONS=true --env GITHUB_EVENT_NAME=push --env GITHUB_REF=refs/heads/main --env PROTOCOL_CODE="sfp-401-nl" inbo/protocols
+
+
+
+#examples
 docker build --no-cache --tag=inbobmk/checklist .
 
 docker build --tag=inbobmk/checklist .
@@ -10,17 +27,4 @@ docker run --rm -it --entrypoint=/bin/bash --env GITHUB_REPOSITORY="inbo/checkli
 
 # run on main
 docker run --rm --env GITHUB_REPOSITORY="inbo/checklist" --env GITHUB_SHA=$(git rev-parse HEAD) --env GITHUB_PAT=$GITHUB_PAT --env INPUT_PATH="." --env ORCID_TOKEN=$ORCID_TOKEN --env CODECOV_TOKEN=$CODECOV_TOKEN --env GITHUB_ACTIONS=true --env GITHUB_EVENT_NAME=push --env GITHUB_REF=refs/heads/main inbobmk/checklist
-
-
-docker build --tag=inbo/protocols .
-
-#entrypoint_check
-docker run --rm --env GITHUB_REPOSITORY="ElsLommelen/protocolsource" --env BRANCH_SOURCE="docker-setup" --env PROTOCOL_CODE="sfp-401-nl" --env INPUT_TOKEN inbo/protocols
-#INPUT_TOKEN is als omgevingsvariabele toegevoegd op laptop
-
-#entrypoint_update
-docker run --rm --env GITHUB_REPOSITORY="ElsLommelen/protocolsource" --env BRANCH_SOURCE="docker-setup" --env PROTOCOL_CODE="sfp-401-nl" --env INPUT_TOKEN inbo/protocols
-
-#entrypoint_website
-docker run --rm --env GITHUB_REPOSITORY_SOURCE="ElsLommelen/protocolsource" --env BRANCH_SOURCE="docker-setup" --env GITHUB_REPOSITORY_DEST="ElsLommelen/protocols" --env INPUT_TOKEN --env GITHUB_ACTIONS=true --env GITHUB_EVENT_NAME=push --env GITHUB_REF=refs/heads/main --env PROTOCOL_CODE="sfp-401-nl" inbo/protocols
 
